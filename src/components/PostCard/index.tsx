278ddaae9b28ebbea2acfda1829107505ca7f8f7
ToyaKasai <kasaitoya@team-lab.com>
2023-02-24 20:27:52 +0900

    feat: add PostCard component

---
+++ src/components/PostCard/index.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import { Link, AppLink } from '@/components/AppLink';
+
+type Props = Link & {
+  postTitle: string;
+  postDescription: string;
+  category: string; // FIXME: type化
+};
+
+export const PostCard: FC<Props> = ({ postTitle, postDescription, category, ...link }) => {
+  return (
+    <AppLink
+      {...link}
+      title={postTitle}
+      className="flex w-full justify-between space-x-4 rounded-lg border border-gray-400 p-4 transition-[border-color] duration-300 hover:border-black dark:border-gray-500 dark:hover:border-white lg:space-x-6 lg:p-6"
+    >
+      <span className="space-y-6 lg:space-y-12">
+        <p className="text-2xl font-bold lg:text-3xl">{postTitle}</p>
+        <p className="text-sm text-gray-500 dark:text-gray-400 lg:text-lg">{postDescription}</p>
+      </span>
+      <p className="self-end">{category}</p>
+    </AppLink>
+  );
+};
