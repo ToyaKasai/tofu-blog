6f6e0a9c826c1415544beeec36daee3c5ebae339
ToyaKasai <kasaitoya@team-lab.com>
2023-04-11 22:13:33 +0900

    feat: 遷移中のプログレスバーを表示する

---
+++ src/hooks/useRouteProgress.ts
@@ -0,0 +1,28 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+import NProgress from 'nprogress';
+
+export const useRouteProgress = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      NProgress.start();
+    };
+
+    const handleRouteChangeComplete = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+    router.events.on('routeChangeError', handleRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+      router.events.off('routeChangeError', handleRouteChangeComplete);
+    };
+  }, [router]);
+};
